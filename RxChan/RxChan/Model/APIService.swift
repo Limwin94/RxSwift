98661c184f9df7ddd48ccc3c37859b24e47677f8
limwin94 <amuguna94@gmail.com>
2020-09-10 00:52:39 +0900

    메인화면 통신시 필요한 EndPoints 추가, Mock Data 추가

---
+++ RxChan/RxChan/Model/APIService.swift
@@ -0,0 +1,15 @@
+//
+//  APIService.swift
+//  RxChan
+//
+//  Created by 임승혁 on 2020/09/10.
+//  Copyright © 2020 임승혁. All rights reserved.
+//
+
+import Foundation
+
+enum EndPoints: String {
+    case main = "https://h3rb9c0ugl.execute-api.ap-northeast-2.amazonaws.com/develop/baminchan/main"
+    case soup = "https://h3rb9c0ugl.execute-api.ap-northeast-2.amazonaws.com/develop/baminchan/soup"
+    case side = "https://h3rb9c0ugl.execute-api.ap-northeast-2.amazonaws.com/develop/baminchan/side"
+}
